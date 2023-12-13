abdf0989fc9163cf70461064dfcf959904635485
WongIngDonyu <wongingdonyu@gmail.com>
2023-12-13 14:45:19 +0300

    some fix2

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ public class Main {
     public static void main(String[] args) {
         MaxHeap<Integer> maxHeap = new MaxHeap<>();
 
-        maxHeap.add(20);
-        maxHeap.add(10);
         maxHeap.add(15);
         maxHeap.add(5);
-        maxHeap.add(12);
-        maxHeap.add(13);
+        maxHeap.add(10);
+        maxHeap.add(30);
+        maxHeap.add(25);
+        maxHeap.add(40);
         maxHeap.printBeautifulTree();
 
         System.out.println("Наивысший элемент: " + maxHeap.peek());
@@ -15,19 +15,19 @@ public class Main {
 
         PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
 
-        priorityQueue.add(20);
-        priorityQueue.add(10);
         priorityQueue.add(15);
         priorityQueue.add(5);
-        priorityQueue.add(12);
-        priorityQueue.add(13);
+        priorityQueue.add(10);
+        priorityQueue.add(30);
+        priorityQueue.add(25);
+        priorityQueue.add(40);
         priorityQueue.printBeautifulQueue();
 
         System.out.println("Максимальный элемент: " + priorityQueue.peek());
         priorityQueue.remove(2);
         System.out.println("Размер очереди: " + priorityQueue.size());
         priorityQueue.printBeautifulQueue();
-        System.out.println("----");
+        System.out.println("--------------------");
         priorityQueue.remove(1);
         priorityQueue.printBeautifulQueue();
     }
